2202e127003e6f9639e29aa73dae759bc2aaeaf4
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-05 11:49:32 -0300

    Update EditarClienteView.vue

--- src/views/EditarClienteView.vue
+++ src/views/EditarClienteView.vue
@@ -34,18 +34,15 @@
             .then(() => router.push({ name: 'inicio'}))
             .catch(error => console.log(error))
     }
-    
 </script>
 
 <template>
     <div>
         <div class="flex justify-end">
-            <RouterLink to="inicio">
-                Volver a Inicio
-            </RouterLink>
+            <RouterLink to="inicio"> Volver a Inicio </RouterLink>
         </div>
 
-        <Heading>{{ titulo }}</Heading>
+        <Heading> {{ titulo }} </Heading>
 
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w-2/3 py-20 px-6">
@@ -57,61 +54,61 @@
                     :value="formData"
                 >
 
-                    <FormKit
-                        type="text"
-                        label="Nombre"
-                        name="Nombre"
-                        placeholder="Nombre del Cliente"
-                        validation="required"
-                        :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio' }"
-                        v-model="formData.Nombre"
-                    />
+                <FormKit
+                    type="text"
+                    label="Nombre"
+                    name="Nombre"
+                    placeholder="Nombre del Cliente"
+                    validation="required"
+                    :validation-messages="{ required: 'El Nombre del Cliente es Obligatorio' }"
+                    v-model="formData.Nombre"
+                />
 
-                    <FormKit
-                        type="text"
-                        label="Apellido"
-                        name="Apellido"
-                        placeholder="Apellido del Cliente"
-                        validation="required"
-                        :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio' }"
-                        v-model="formData.Apellido"
-                    />
+                <FormKit
+                    type="text"
+                    label="Apellido"
+                    name="Apellido"
+                    placeholder="Apellido del Cliente"
+                    validation="required"
+                    :validation-messages="{ required: 'El Apellido del Cliente es Obligatorio' }"
+                    v-model="formData.Apellido"
+                />
 
-                    <FormKit
-                        type="text"
-                        label="Email"
-                        name="Email"
-                        placeholder="Email del Cliente"
-                        validation="required|email"
-                        :validation-messages="{ required: 'El Email del Cliente es Obligatorio' , email: 'Coloca un Email válido'}"
-                        v-model="formData.Email"
-                    />
+                <FormKit
+                    type="text"
+                    label="Email"
+                    name="Email"
+                    placeholder="Email del Cliente"
+                    validation="required|email"
+                    :validation-messages="{ required: 'El Email del Cliente es Obligatorio' , email: 'Coloca un Email válido'}"
+                    v-model="formData.Email"
+                />
 
-                    <FormKit
-                        type="text"
-                        label="Teléfono"
-                        name="Teléfono"
-                        placeholder="Teléfono: XXX-XXX-XXXX"
-                        validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
-                        :validation-messages="{ matches: 'El Formato no es válido'}"
-                        v-model="formData.Teléfono"
-                    />
+                <FormKit
+                    type="text"
+                    label="Teléfono"
+                    name="Teléfono"
+                    placeholder="Teléfono: XXX-XXX-XXXX"
+                    validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                    :validation-messages="{ matches: 'El Formato no es válido'}"
+                    v-model="formData.Teléfono"
+                />
 
-                    <FormKit
-                        type="text"
-                        label="Empresa"
-                        name="Empresa"
-                        placeholder="Empresa del Cliente"
-                        v-model="formData.Empresa"
-                    />
+                <FormKit
+                    type="text"
+                    label="Empresa"
+                    name="Empresa"
+                    placeholder="Empresa del Cliente"
+                    v-model="formData.Empresa"
+                />
                     
-                    <FormKit
-                        type="text"
-                        label="Puesto"
-                        name="Puesto"
-                        placeholder="Puesto del Cliente"
-                        v-model="formData.Puesto"
-                    />
+                <FormKit
+                    type="text"
+                    label="Puesto"
+                    name="Puesto"
+                    placeholder="Puesto del Cliente"
+                    v-model="formData.Puesto"
+                />
 
                 </FormKit>
             </div>
