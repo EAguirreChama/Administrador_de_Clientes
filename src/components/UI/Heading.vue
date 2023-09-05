3a557a689487145f8d82585339a1eb8a3878aca9
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-05 11:43:58 -0300

    Update Heading.vue

--- src/components/UI/Heading.vue
+++ src/components/UI/Heading.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1 class="text-4xl font-extrabold text-slate-600"><slot></slot></h1>
+    <h1 class="text-4xl font-extrabold text-slate-600">
+        <slot></slot>
+    </h1>
 </template>
