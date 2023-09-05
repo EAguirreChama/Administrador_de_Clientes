687da5cdefb8204dd1ed65ffaca076dbdda3ed0b
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-05 11:47:26 -0300

    Update ClientesView.vue

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -40,31 +40,29 @@
             })
             .catch(error => console.log(error))
     }
-
 </script>
 
 <template>
     <div>
         <div class="flex justify-end">
-            <RouterLink to="agregar-cliente">
-                Agrgar Cliente
-            </RouterLink>
+            <RouterLink to="agregar-cliente"> Agrgar Cliente </RouterLink>
         </div>
 
-        <Heading>{{ titulo }}</Heading>
+        <Heading> {{ titulo }} </Heading>
 
         <div v-if="existenClientes" class="flow-root mx-auto  mt-10 p-5 bg-white shadow">
             <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
                     <table class="min-w-full divide-y divide-gray-300">
                         <thead>
-                        <tr>
-                            <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Nombre</th>
-                            <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Empresa</th>
-                            <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Estado</th>
-                            <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Acciones</th>
-                        </tr>
+                            <tr>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Nombre</th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Empresa</th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Estado</th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Acciones</th>
+                            </tr>
                         </thead>
+                        
                         <tbody class="divide-y divide-gray-200 bg-white">
                             <Cliente
                                 v-for="cliente in clientes"
