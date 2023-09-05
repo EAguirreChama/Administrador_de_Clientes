a9955d5d3e350004939b0deec69a14f122216767
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-05 11:52:02 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,11 @@
 </script>
 
 <template>
-
   <header class="bg-slate-800">
     <div class="mx-auto max-w-6xl py-10">
-      <h1 class="text-4xl font-extrabold text-white">
-        Administrador de Clientes
-      </h1>
+      <h1 class="text-4xl font-extrabold text-white"> Administrador de Clientes </h1>
     </div>
   </header>
 
-  <main class="max-w-6xl mx-auto mt-10">
-    <RouterView/>
-  </main>
-
+  <main class="max-w-6xl mx-auto mt-10"> <RouterView/> </main>
 </template>
-
-
