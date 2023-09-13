36dfd0f7ef81e91cd04207189bb0efc2d4983d25
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-13 14:26:00 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import { plugin, defaultConfig } from '@formkit/vue'
 import config from '../formkit.config'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(plugin, defaultConfig(config))
-
 app.mount('#app')
