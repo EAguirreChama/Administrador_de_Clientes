eb1e8d77834505f0b1495500865222187eda565b
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-13 14:25:51 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,5 @@
       <h1 class="text-4xl font-extrabold text-white"> Administrador de Clientes </h1>
     </div>
   </header>
-
   <main class="max-w-6xl mx-auto mt-10"> <RouterView/> </main>
 </template>
